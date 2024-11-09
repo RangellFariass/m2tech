84aa48cf1ab0dcb407970b3e1ea502468ede2116
RangellFariass <rangellfariass@gmail.com>
2024-11-08 22:41:23 -0300

    novas funções

--- src/app/ticket.tsx
+++ src/app/ticket.tsx
@@ -1,11 +1,69 @@
-import { Credential } from "@/components/credential"
-import { Header } from "@/components/header"
-import { StatusBar, View } from "react-native"
+import { useState } from "react"
+import { StatusBar, Text, View, ScrollView, TouchableOpacity, Alert } from "react-native"
+import { FontAwesome } from "@expo/vector-icons"
+import * as ImagePicker from "expo-image-picker"
 
+import { colors } from "@/styles/colors"
+
+import { Header } from "@/components/header"
+import { Button } from "@/components/button"
+import { Credential } from "@/components/credential"
+ 
 export default function Ticket() {
-    return <View className="flex-1 bg-purple-1000">
-        <StatusBar barStyle="light-content" />
-        <Header title="Minha Credencial" />
-        <Credential />
-    </View>
+const [image, setImage] = useState("")
+
+async function handleSelectImage() {
+    try {
+       const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 4],
+       }) 
+
+       if (result.assets) {
+        setImage(result.assets[0].uri)
+       }
+    } catch (error) {
+        console.log(error)
+        Alert.alert("Foto", "Não foi possivel selecionar a imagem.")
+    }
+}
+
+    return (
+        <View className="flex-1 bg-purple-1000">
+            <StatusBar barStyle="light-content" />
+            <Header title="Minha Credencial" />
+            
+            <ScrollView 
+            className="-mt-28 -z-10" 
+            contentContainerClassName="px-8 pb-8"
+            showsHorizontalScrollIndicator={false}
+            >
+            <Credential image={image} onChangeAvatar={handleSelectImage}/>
+
+            <FontAwesome 
+            name="angle-double-down" 
+            color={colors.gray[300]}
+            size={24} 
+            className="self-center my-6"
+            />
+
+            <Text className="text-white font-bold text-2xl mt-4">
+               Compartilhar credencial 
+            </Text>
+
+            <Text className="text-white font-regular text-base mt-1 mb-6">
+               Mostre ao mundo que você vai participar do Mr2 Tech!
+            </Text>
+
+            <Button title="Compartilhar"/>
+
+            <TouchableOpacity activeOpacity={0.7} className="mt-10">
+                <Text className="text-base text-white font-bold text-center">
+                    Remover Ingresso
+                </Text>
+            </TouchableOpacity>
+          </ScrollView>
+        </View>
+    )
 }
--- src/components/credential.tsx
+++ src/components/credential.tsx
@@ -1,7 +1,14 @@
 import { 
     View, Image, ImageBackground, Text, TouchableOpacity } from "react-native";
+    import { Feather } from "@expo/vector-icons"
+    import { colors } from "@/styles/colors";
 
-export function Credential(){
+    type Props = {
+      image?: string
+      onChangeAvatar?: () => void
+    }
+
+export function Credential({ onChangeAvatar, image }: Props){
     return <View className="w-full self-stretch items-center">
         <Image 
         source={require("@/assets/ticket/band.png")} 
@@ -21,8 +28,21 @@ export function Credential(){
             <View className="w-40 h-40 bg-black rounded-full"></View>
         </ImageBackground>
 
-        <Image source={{ uri: "https://github.com/rangellfariass.png" }} className="w-36 h-36 rounded-full -mt-24"
+        {
+          image ? (
+            <TouchableOpacity activeOpacity={0.9} onPress={onChangeAvatar}>
+        <Image source={{ uri: image }} className="w-36 h-36 rounded-full -mt-24"
         />
+        </TouchableOpacity>
+          ) : (
+          <TouchableOpacity
+          activeOpacity={0.9}
+          className="w-36 h-36 rounded-full -mt-24 bg-gray-400 items-center justify-center"
+          onPress={onChangeAvatar}
+          >
+        <Feather name="camera" color={colors.purple[200]} size={32}/>
+        </TouchableOpacity>
+        )}
 
         <Text className="font-bold text-2xl text-zinc-50 mt-4">
             Rangel Farias
